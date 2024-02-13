697eb5b3abeef29afc549b4b1878bed3552bfc4a
JhonatanRian <jhonatanrian.jr@gmail.com>
2024-02-13 19:56:01 -0300

    🔨 feat: add codes for logs

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/JhonatanRian/go-api/router"
+import (
+	"fmt"
+
+	"github.com/JhonatanRian/go-api/config"
+	"github.com/JhonatanRian/go-api/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("go-api")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Failed to load config: %v", err)
+		return
+	}
 	router.Initialize()
 }
