4805103caacf50612d5c3e6cb42e7630d64cff74
JhonatanRian <jhonatanrian.jr@gmail.com>
2024-02-15 20:09:52 -0300

    🔨 feat: router

--- router/routes.go
+++ router/routes.go
@@ -3,16 +3,23 @@ package router
 import (
 	"github.com/JhonatanRian/go-api/handlers"
 	"github.com/gin-gonic/gin"
+	docs "github.com/JhonatanRian/go-api/docs"
+	swaggerfiles "github.com/swaggo/files"
+   	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeOpeningRoutes(router *gin.Engine) {
 	handlers.InitializeHandler()
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
-		v1.POST("/openings", handlers.CreateOpening)
-		// v1.POST("/openings", openinghandlers.CreateOpening)
-		// v1.GET("/openings", openinghandlers.CreateOpening)
-		// v1.PUT("/openings", openinghandlers.CreateOpening)
-		// v1.DELETE("/openings", openinghandlers.CreateOpening)
+		v1.GET("/openings", handlers.ListOpeningHandler)
+		v1.POST("/openings", handlers.CreateOpeningHandler)
+		v1.GET("/openings/:id", handlers.DetailOpeningHandler)
+		v1.PUT("/openings/:id", handlers.UpdateOpeningHandler)
+		v1.DELETE("/openings/:id", handlers.DeleteOpeningHandler)
 	}
+
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
